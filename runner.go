55e61e2ceef8d42a84a66857e7e911762d4f56ff
fschnko <fschnko@gmail.com>
2017-11-13 22:26:42 +0200

    Add copyright headers

--- runner.go
+++ runner.go
@@ -1,3 +1,7 @@
+// Copyright © 2017 Artem Feschenko. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package repeat
 
 import (
